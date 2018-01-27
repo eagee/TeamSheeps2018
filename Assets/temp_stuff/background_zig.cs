b88adb6c50af67679c88099a65ad23009a80ed71
Joe O'Rourke <GHJoe@dev-sean.com>
2018-01-26 20:14:02 -0800

    temp background

---
+++ Assets/temp_stuff/background_zig.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background_zig : MonoBehaviour {
+
+    public Vector3 drift_vector = Vector3.forward;
+    public Vector3 spin_vector = Vector3.zero;
+    Vector3 original_position;
+
+	// Use this for initialization
+	void Start () {
+        original_position = transform.position;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = original_position;
+        transform.Translate(drift_vector * Mathf.Sin(Time.time));
+        transform.Rotate(spin_vector * Mathf.Cos(Time.time));
+	}
+}
